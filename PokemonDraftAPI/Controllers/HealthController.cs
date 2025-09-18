eeb344e70e98f6bc8cb600d8b879514af35f4ed1
Adrián Estrada González <adrian.estrada2001@gmail.com>
2025-09-18 17:28:35 +0200

    health

---
+++ PokemonDraftAPI/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PokemonDraftAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "ok", timestamp = DateTime.UtcNow });
+        }
+    }
+}
