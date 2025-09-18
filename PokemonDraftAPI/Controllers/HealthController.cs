d2315e4a4b8fc2e010762706ba88b60f0a613f58
Adrián Estrada González <adrian.estrada2001@gmail.com>
2025-09-18 18:14:47 +0200

    head

--- PokemonDraftAPI/Controllers/HealthController.cs
+++ PokemonDraftAPI/Controllers/HealthController.cs
@@ -6,7 +6,8 @@ namespace PokemonDraftAPI.Controllers
     [Route("api/[controller]")]
     public class HealthController : ControllerBase
     {
-        [HttpGet]
+        // Permite tanto GET como HEAD
+        [HttpGet, HttpHead]
         public IActionResult Get()
         {
             return Ok(new { status = "ok", timestamp = DateTime.UtcNow });
